7059efed0e6dd8b4aa395707c563192d61ff715c
zkazharan <azka.azharan@gmail.com>
2023-12-21 16:13:32 +0700

    mobile responsive

--- src/app/history/Historybranch.js
+++ src/app/history/Historybranch.js
@@ -42,43 +42,75 @@ export default function Historybranch() {
   ];
 
   return (
-    <div className="relative w-screen min-h-screen py-16 flex justify-center items-center bg-[#2B2B2B]">
-      <Image src={Logo} className="absolute max-w-[640px]" alt="logo" />
-      <div className="relative z-10 flex justify-center">
-        <div className="absolute bg-white h-full w-[6px] flex flex-col justify-between items-center">
-          <div className="relative -top-1 w-5 h-5 rounded-full bg-white"></div>
-          {cardData.map((item, index) => (
-            <div
-              key={"dot" + index}
-              className={`w-5 h-5 rounded-full ${color[item.colorIndex]}`}
-            ></div>
-          ))}
-          <div className="relative -bottom-1 w-5 h-5 rounded-full bg-white"></div>
-        </div>
-        <div className="flex flex-col py-8">
-          {cardData.map((item, index) => (
-            <div
-              key={index}
-              className={
-                "w-[900px] flex items-center " +
-                (index % 2 == 0 ? "justify-end " : "justify-start ") +
-                (index !== 0 ? "-mt-[60px]" : "")
-              }
-            >
+    <>
+      <div className="relative w-screen min-h-screen py-16 justify-center items-center bg-[#2B2B2B] hidden lg:flex">
+        <Image src={Logo} className="absolute max-w-[640px]" alt="logo" />
+        <div className="relative z-10 flex justify-center">
+          <div className="absolute bg-white h-full w-[6px] flex flex-col justify-between items-center">
+            <div className="relative -top-1 w-5 h-5 rounded-full bg-white"></div>
+            {cardData.map((item, index) => (
               <div
-                className={`relative p-3 corner-border ${
+                key={"dot" + index}
+                className={`w-5 h-5 rounded-full ${color[item.colorIndex]}`}
+              ></div>
+            ))}
+            <div className="relative -bottom-1 w-5 h-5 rounded-full bg-white"></div>
+          </div>
+          <div className="flex flex-col py-8">
+            {cardData.map((item, index) => (
+              <div
+                key={index}
+                className={
+                  "w-[900px] flex items-center " +
+                  (index % 2 == 0 ? "justify-end " : "justify-start ") +
+                  (index !== 0 ? "-mt-[60px]" : "")
+                }
+              >
+                <div
+                  className={`relative p-3 corner-border ${
+                    beforeBorderColor[item.colorIndex]
+                  } ${afterBorderColor[item.colorIndex]}`}
+                >
+                  <div className="bg-[#807D7D] px-6 py-4 text-white w-[360px] h-[150px] flex flex-col gap-2 rounded-lg shadow-[7px_7px_4px_0px_rgba(31,31,31,0.25)]">
+                    <h1 className="text-3xl font-medium">{item.title}</h1>
+                    <p className="leading-5 tracking-wide text-sm">{item.desc}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="relative w-screen min-h-screen py-16 justify-center items-center bg-[#2B2B2B] flex lg:hidden">
+        <Image src={Logo} className="absolute max-w-[320px]" alt="logo" />
+        <div className="relative z-10 flex gap-4">
+          <div className="bg-white w-[6px] flex flex-col justify-around items-center py-8">
+            <div className="absolute -top-1 w-5 h-5 rounded-full bg-white"></div>
+            {cardData.map((item, index) => (
+              <div
+                key={"dot" + index}
+                className={`w-5 h-5 rounded-full ${color[item.colorIndex]}`}
+              ></div>
+            ))}
+            <div className="absolute -bottom-1 w-5 h-5 rounded-full bg-white"></div>
+          </div>
+          <div className="flex flex-col py-8">
+            {cardData.map((item, index) => (
+              <div
+                key={"card" + index}
+                className={`relative p-3 corner-border my-3 ${
                   beforeBorderColor[item.colorIndex]
                 } ${afterBorderColor[item.colorIndex]}`}
               >
-                <div className="bg-[#807D7D] px-6 py-4 text-white w-[360px] h-[150px] flex flex-col gap-2 rounded-lg shadow-[7px_7px_4px_0px_rgba(31,31,31,0.25)]">
+                <div className="bg-[#807D7D] px-4 py-4 text-white w-[280px] h-[180px] flex flex-col gap-2 rounded-lg shadow-[7px_7px_4px_0px_rgba(31,31,31,0.25)]">
                   <h1 className="text-3xl font-medium">{item.title}</h1>
                   <p className="leading-5 tracking-wide text-sm">{item.desc}</p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
